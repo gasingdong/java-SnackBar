bd52eb1aa8ec66698baf380933ea4708ba407f53
Brandon <gasingdong@gmail.com>
2019-09-01 19:44:37 -0700

    finish transactions

--- src/snackbar/SnackBar.java
+++ src/snackbar/SnackBar.java
@@ -24,5 +24,34 @@ public class SnackBar {
 		System.out.println(jane.getName() + " buys 3 " + soda.getName());
 		System.out.println(jane.getName() + " now has " + jane.getCash());
 		System.out.println("There are " + soda.getQuantity() + " " + soda.getName() + " remaining");
+
+		jane.buy(pretzel.getTotalCost(1));
+		pretzel.buy(1);
+		System.out.println(jane.getName() + " buys 1 " + pretzel.getName());
+		System.out.println(jane.getName() + " now has " + jane.getCash());
+		System.out.println("There are " + pretzel.getQuantity() + " " + pretzel.getName() + " remaining");
+
+		bob.buy(soda.getTotalCost(2));
+		soda.buy(2);
+		System.out.println(bob.getName() + " buys 2 " + soda.getName());
+		System.out.println(bob.getName() + " now has " + bob.getCash());
+		System.out.println("There are " + soda.getQuantity() + " " + soda.getName() + " remaining");
+
+		jane.addCash(10);
+		System.out.println(jane.getName() + " found 10 dollars. Total cash on hand is now " + jane.getCash());
+
+		jane.buy(chocolateBar.getTotalCost(1));
+		chocolateBar.buy(1);
+		System.out.println(jane.getName() + " buys 1 " + chocolateBar.getName());
+		System.out.println(jane.getName() + " now has " + jane.getCash());
+		System.out.println("There are " + chocolateBar.getQuantity() + " " + chocolateBar.getName() + " remaining");
+
+		pretzel.add(12);
+		System.out.println("Added 12 " + pretzel.getName() + ". Current stock is now " + pretzel.getQuantity());
+
+		bob.buy(pretzel.getTotalCost(3));
+		System.out.println(bob.getName() + " buys 3 " + pretzel.getName());
+		System.out.println(bob.getName() + " now has " + bob.getCash());
+		System.out.println("There are " + pretzel.getQuantity() + " " + pretzel.getName() + " remaining");
 	}
 }
